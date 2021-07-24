15defa3a125c3b36f528982fc30a9d646ff061e9
Josimar16 <josimarjr479@gmail.com>
2021-07-24 12:38:42 -0300

    feat: :sparkles: Criando pagina de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,80 @@
 import Head from 'next/head';
-import { Text } from '@chakra-ui/react';
+import { Flex, Button } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Input } from '../components/Form/Input';
+import { useAuth } from '../hooks/Auth';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail é obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha é obrigatório').min(6, 'No mínimo 6 caracteres')
+})
 
 export default function Home() {
+  const { signIn } = useAuth();
+
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (data) => {
+    await signIn(data);
+  }
+
   return (
     <>
-      <Text>Ola mundo</Text>
+      <Head>
+        <title>Login | NextAuth</title>
+        <meta name="description" content="Tela de login do app" />
+      </Head>
+      <Flex
+        width="100vw"
+        height="100vh"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Flex
+          as="form"
+          width="100%"
+          maxWidth={360}
+          backgroundColor="gray.800"
+          padding="8"
+          borderRadius={8}
+          flexDirection="column"
+          onSubmit={handleSubmit(handleSignIn)}
+        >
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            error={formState.errors.email}
+            {...register('email')}
+          />
+
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+            error={formState.errors.password}
+            {...register('password')}
+          />
+          <Button
+            type="submit"
+            marginTop="6"
+            colorScheme="green"
+            size="lg"
+            isLoading={formState.isSubmitting}
+          >
+            Entrar
+          </Button>
+        </Flex>
+      </Flex>
     </>
   )
 }
