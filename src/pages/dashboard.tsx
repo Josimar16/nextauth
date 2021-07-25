cf67b64e1aed0952aa85dba6a1c9fbdd9c61e792
Josimar16 <josimarjr479@gmail.com>
2021-07-25 07:17:15 -0300

    feat: :sparkles: Adicionando validação de autenticação

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,6 @@
+import { GetServerSidePropsContext } from "next";
 import { useAuth } from "../hooks/Auth";
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useAuth();
@@ -7,3 +9,9 @@ export default function Dashboard() {
     <h1>Dashboard: {user.email}</h1>
   );
 }
+
+export const getServerSideProps = withSSRAuth(async (context: GetServerSidePropsContext) => {
+  return {
+    props: {}
+  }
+});
