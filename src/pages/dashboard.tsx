a8a9b236a8eecaec09a3adaf1ce0e058fc03b3d0
Josimar16 <josimarjr479@gmail.com>
2021-07-26 23:16:47 -0300

    feat: :sparkles: Validando o dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,15 @@
 import { GetServerSidePropsContext } from "next";
-import { useAuth } from "../hooks/Auth";
+import { useAuth } from "../hooks/useAuth";
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useAuth();
+
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list']
+  });
 
   return (
     <h1>Dashboard: {user.email}</h1>
