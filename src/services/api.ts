d105b4b831338bd4ce20571057cd3231197e7232
Josimar16 <josimarjr479@gmail.com>
2021-07-26 23:18:01 -0300

    refactor: :recycle: Refatorando caminho do novo caminho do hook

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies';
-import { signOut } from '../hooks/Auth';
+import { signOut } from '../hooks/useAuth';
 import { AuthTokenError } from './errors/AuthTokenError';
 
 let isRefreshing = false;
